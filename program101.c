98d519eb8a81015d572497c6f4e32ece42eb03ef
deepthi-it19 <deepthim.it19@bitsathy.ac.in>
2019-10-01 15:51:47 +0530

    first commit

---
+++ program101.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int row,column;
+   for(row=1;row<=5;row++)
+   {
+       for(column=1;column<=row;column++)
+       {
+           printf("%d",column);
+      
+       }
+       printf("\n");
+   
+      
+   }
+return 0;
+}
+
