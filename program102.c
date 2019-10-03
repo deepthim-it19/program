646eccb8c820a03e3eb8e7b50834b17850f246e9
deepthi-it19 <deepthim.it19@bitsathy.ac.in>
2019-10-03 09:55:29 +0530

    first commit

---
+++ program102.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int x,y;
+printf("enter an train length=");
+scanf("%d",&x);
+printf("enter an bridge length=");
+scanf("%d",&y);
+printf("total distance travelled:%d",x+y);
+return 0;
+}
+
